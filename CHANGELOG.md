4b8862ba0feb3229b8d390523e733cd8e18e2f07
Julien Loizelet <julienloizelet@okaeli.com>
2024-06-27 10:53:31 +0900

    feat(*): Init project (#1)
    * feat(*): Add connector skeleton
    
    * feat(*): Import for 1 IP
    
    * feat(*): Avoid database request for speedup import
    
    * feat(*): Use batch to send bundles to OpenCTI
    
    * feat(config): Make config variable consistant
    
    * feat(*): Store last run on first loop to avoid multiple works to run
    
    * feat(config): Rename config variable for consistency with other connectors
    
    * fix(None value): Handle None value returned by CTI
    
    * feat(errors): Improve error management
    
    * feat(*): Add Ipv6 support
    
    * feat(*): Set is_running state to avoid concurrent runs
    
    * feat(acl): Handle max observable tlp
    
    * feat(*): Add enrichment threshold per import
    
    * feat(log): Improve date logs
    
    * feat(log): Improve date logs
    
    * feat(log): Improve date logs
    
    * style(*): Refactor importer
    
    * feat(*): Add settings to allow light enrichment
    
    * feat(*): Remove option to add external ref and always add it
    
    * fix(import): Avoid multiple run even if not first run
    
    * test(*): Add tests and ci actions
    
    * docs(*): Add docs
    
    * docs(*): Add TOC
    
    * fix(import): Fix division by zero error
    
    * feat(*): Add log for total number of enrichments
    
    * feat(*): Add log for current number of enrichments
    
    * docs(user guide): Add benchmarks

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## SemVer public API
+
+The [public API](https://semver.org/spec/v2.0.0.html#spec-item-1)  for this project is defined by the set of 
+functions provided by the `src` folder and the following files: `docker-compose.yml`, `Dockerfile`, `entrypoint.sh`
+
+---
+
+
+## [0.0.1](https://github.com/crowdsecurity/cs-opencti-external-import-connector/releases/tag/v1.0.0) - 2024-??-??
+
+- Initial release
---
+++ src/crowdsec/client.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""CrowdSec client module."""
+
+import itertools
+from urllib.parse import urljoin
+import requests
+from time import sleep
+from dataclasses import dataclass
+from pycti import OpenCTIConnectorHelper
+
+
+class QuotaExceedException(Exception):
+    pass
+
+
+@dataclass
+class CrowdSecClient:
+    """CrowdSec client."""
+
+    helper: OpenCTIConnectorHelper
+    url: str
+    api_key: str
+
+    @staticmethod
+    def download_file(url: str, destination: str):
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(destination, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        return destination
+
+    def get_crowdsec_dump(self):
+        for i in itertools.count(1, 1):
+            resp = requests.get(
+                urljoin(self.url, f"dump"),
+                headers={
+                    "x-api-key": self.api_key,
+                    "User-Agent": "crowdsec-import-opencti/v0.0.1",
+                },
+            )
+            if resp.status_code == 429:
+                raise QuotaExceedException(
+                    (
+                        "Quota exceeded for CrowdSec CTI API. "
+                        "Please visit https://www.crowdsec.net/pricing to upgrade your plan."
+                    )
+                )
+            elif resp.status_code == 200:
+                return resp.json()
+            else:
+                self.helper.log_debug(f"CrowdSec CTI request {resp.url}")
+                self.helper.log_debug(f"CrowdSec CTI headers {resp.request.headers}")
+                self.helper.log_info(f"CrowdSec CTI response {resp.text}")
+                self.helper.log_warning(
+                    f"CrowdSec CTI returned {resp.status_code} response status code. Retrying..."
+                )
+            sleep(2**i)
