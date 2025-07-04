c4ec04f2bf545ccb7de039694bc8c98aae0cc2a1
Julien Loizelet <julienloizelet@okaeli.com>
2025-07-04 17:40:53 +0900

    style(*): Pass through code format tools

--- src/crowdsec/tests/test_builder.py
+++ src/crowdsec/tests/test_builder.py
@@ -7,10 +7,9 @@ import unittest
 from unittest.mock import MagicMock
 
 import stix2
-from dateutil.parser import parse
-
 from crowdsec.builder import CrowdSecBuilder
 from crowdsec.constants import FAKE_INDICATOR_ID
+from dateutil.parser import parse
 
 
 def load_file(filename: str):
